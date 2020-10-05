3234a2db2f3ef775b1aea054e860cfb1a098b238
TAREK ABDEL-RAHMAN <47148243+tarekarahman@users.noreply.github.com>
2020-10-04 22:25:16 -0400

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -47,6 +47,46 @@ bool isPalindromic(string s){
         // x=x/10;
         return isNegative? x*-1 : x;
         }
+                    int stringToInt(string s, int baseFrom){
+                 int x;
+                 bool isNegative = false;
+                 if(s[0]=='-'){
+                     isNegative = true;
+                     }
+                     
+                 x=accumulate(s.begin()+ isNegative, s.end(), 0, [&baseFrom](int sum, char c){
+                     return sum*baseFrom + (isdigit(c) ? c - '0' : c - 'A' + 10);
+                     });
+                return isNegative? x*-1 : x;
+                }
+                
+            string intToString(int x, int baseTo){
+                string s;
+                bool isNegative = false;
+                if(x<0){
+                 isNegative =true;
+                    }
+                    
+                while(x){
+                s+= abs(x % baseTo)+ (abs(x % baseTo) >= 10 ? 'A'-10 : '0') ;
+                x=x/baseTo;
+                }
+                s+= isNegative ? "-":"";
+                return {s.rbegin(),s.rend()};
+                }
+        string convertBase(const string& s, int baseFrom, int baseTo){
+            
+            // int stringToInt(string s, int baseFrom);
+            // string intToString(int x, int baseTo);
+            
+            auto x = stringToInt(s, baseFrom);
+            cout<<"x is "<< x <<endl;
+            
+            return intToString(x, baseTo);
+            
+
+            
+            }
     
 int main()
 {
@@ -63,6 +103,10 @@ cout<<s1<<endl;
 
 string sReverse{s.rbegin(),s.rend()};
 cout<<sReverse<<endl;
+
+int b1=7, b2=13;
+string s3= "615";
+cout<< convertBase(s3,b1,b2);
 
 
 
