32f5d12a67325b466b1ce9a5e4f69b4d6a3466ff
TAREK ABDEL-RAHMAN <47148243+tarekarahman@users.noreply.github.com>
2020-10-09 00:48:27 -0400

    Update hashTables.cpp

--- hashTables.cpp
+++ hashTables.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <cassert>
 #include <numeric>
+#include <list>
 
 using namespace std;
 
@@ -99,6 +100,60 @@ void SimpleTest() {
   assert(isLetterConstFromMagazine("", ""));
 }
 
+class LRUcache{
+public:
+    LRUcache(size_t capacity){_capacity=capacity;}
+    int Lookup(int isbn){
+    if(auto it =hashTable.find(isbn); it == hashTable.end()){
+      return -1;
+    }
+      else{
+      auto price = it->second.second;
+      moveToFront(isbn,it);
+      return price;
+      }
+    }
+  bool Erase(int isbn){
+    auto it = hashTable.find(isbn);
+    if(it == hashTable.end()){
+    return false;
+    }
+    else{
+    _lru_queue.erase(it->second.first);  
+    hashTable.erase(it);
+    return false;
+    }
+  }
+  void Insert(int isbn, int price){
+  auto it = hashTable.find(isbn);
+    if(it == hashTable.end()){
+    moveToFront(isbn,it);    
+    }
+    else{
+     if(hashTable.size() == (unsigned) _capacity){
+     //auto itToPop = _lru_cache.end();
+       hashTable.erase(_lru_queue.back());
+       _lru_queue.pop_back();
+     }
+      _lru_queue.push_front(isbn);
+      hashTable[isbn]={_lru_queue.begin(),price};
+    }
+  }
+  private:
+  typedef unordered_map <int, pair<list<int>::const_iterator, int>> Table;
+  list<int> _lru_queue;
+  Table hashTable;
+  int _capacity;
+  
+  void moveToFront(int isbn, const Table::iterator it){
+  _lru_queue.erase(it->second.first);
+   _lru_queue.emplace_front(isbn);
+    it->second.first=_lru_queue.begin();
+    
+    
+  }
+};
+
 // To execute C++, please define "int main()"
 int main() {
   vector<string> words = { "debitcard", "elvis", "silent", "badcredit", "lives", "freedom", "listen", "levis"
@@ -118,9 +173,10 @@ int main() {
   }
   
   vector<string> paragraph{"All", "work", "and", "no", "play", "makes", "for", "no", "work", "no"};
-  cout<<findNearestRepitition(paragraph)<<endl;
+  //cout<<findNearestRepitition(paragraph)<<endl;
   assert(findNearestRepitition(paragraph)==2);
-  
+  size_t capacity=10;
+  LRUcache lru(capacity);
   return 0;
 }
 
